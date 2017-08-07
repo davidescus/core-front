005f7675feeb152abdba7cac5e135daad644cf5e
davidescus <rahthman_s@yahoo.com>
2017-08-07 13:47:43 +0000

    Add portlet for general settings

--- files/pages/site.php
+++ files/pages/site.php
@@ -5,32 +5,183 @@
 
         <div class="row">
             <div class="col-sm-12">
-                <div class="portlet">
+                <div class="col-sm-6">
+                    <select class="form-control site-selection select2">
+                        <option value=" ">Select Website</option>
+                        <option value="1">Option 1</option>
+                        <option value="2">Option 2</option>
+                    </select>
+                </div>
+                <div class="col-sm-6">
+                    <div class="actions pull-right">
+                        <button class="btn green">Add New</button>
+                        <button class="btn red">Delete</button>
+                        <button class="btn green">Save</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h1 class="page-title">
+        GetMyBet.com
+        </h1>
+
+        <div class="row">
+            <div class="col-md-12">
+                <!-- BEGIN Portlet PORTLET-->
+                <div class="portlet light bordered">
                     <div class="portlet-title">
+                        <div class="caption">
+                            <i class="icon-speech"></i>
+                            <span class="caption-subject bold uppercase"> General Settings</span>
+                        </div>
+                    </div>
+                    <div class="portlet-body">
                         <div class="row">
-                            <div class="col-sm-6">
+                            <div class="col-md-4">
+                                <h4>General</h4>
                                 <div class="form-group">
-                                    <select class="form-control site-selection select2">
-                                        <option value="">Select Website</option>
-                                        <option value="">Option 1</option>
-                                        <option value="">Option 2</option>
+                                    <label>Website name</label>
+                                    <input type="text" class="form-control" placeholder="">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Website URL</label>
+                                    <input type="text" class="form-control" placeholder="">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Default Select</label>
+                                    <select class="form-control">
+                                        <option>Option 1</option>
+                                        <option>Option 2</option>
+                                        <option>Option 3</option>
+                                        <option>Option 4</option>
+                                        <option>Option 5</option>
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Thank you page parameter</label>
+                                    <input type="text" class="form-control" placeholder="">
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h4>SMTP</h4>
+                                <div class="form-group">
+                                    <label>Host</label>
+                                    <input type="text" class="form-control" placeholder="">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>User</label>
+                                    <input type="text" class="form-control" placeholder="">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="text" class="form-control" placeholder="">
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Encryption</label>
+                                    <select class="form-control">
+                                        <option>Option 1</option>
+                                        <option>Option 2</option>
+                                        <option>Option 3</option>
+                                        <option>Option 4</option>
+                                        <option>Option 5</option>
                                     </select>
                                 </div>
                             </div>
-                            <div class="col-sm-6">
-                                <div class="actions pull-right">
-                                    <button class="btn green">Add New</button>
-                                    <button class="btn red">Delete</button>
-                                    <button class="btn green">Save</button>
+
+                            <div class="col-md-4">
+                                <h4>Results </h4>
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <h4 class="">Win</h4>
+                                    </div>
+
+                                    <div class="col-md-5">
+                                        <div class="form-group">
+                                            <label>Text</label>
+                                            <input type="text" class="form-control" placeholder="">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-5">
+                                         <div class="form-group">
+                                            <label>Class</label>
+                                            <input type="text" class="form-control" placeholder="">
+                                        </div>
+                                    </div>
                                 </div>
+
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <h4 class="">Loss</h4>
+                                    </div>
+
+                                    <div class="col-md-5">
+                                        <div class="form-group">
+                                            <label>Text</label>
+                                            <input type="text" class="form-control" placeholder="">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-5">
+                                         <div class="form-group">
+                                            <label>Class</label>
+                                            <input type="text" class="form-control" placeholder="">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <h4 class="">Draw</h4>
+                                    </div>
+
+                                    <div class="col-md-5">
+                                        <div class="form-group">
+                                            <label>Text</label>
+                                            <input type="text" class="form-control" placeholder="">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-5">
+                                         <div class="form-group">
+                                            <label>Class</label>
+                                            <input type="text" class="form-control" placeholder="">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <h4 class="">Postpone</h4>
+                                    </div>
+
+                                    <div class="col-md-5">
+                                        <div class="form-group">
+                                            <label>Text</label>
+                                            <input type="text" class="form-control" placeholder="">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-5">
+                                         <div class="form-group">
+                                            <label>Class</label>
+                                            <input type="text" class="form-control" placeholder="">
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                     </div>
-
-                    <div class="portlet-title">
-                    </div>
-
                 </div>
+                <!-- END Portlet PORTLET-->
             </div>
         </div>
 
