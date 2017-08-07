c695bec4d4ba43b1dabcebfdef95538d9bd56253
davidescus <rahthman_s@yahoo.com>
2017-08-07 11:10:46 +0000

    Add dropdown and few buttons

--- files/pages/site.php
+++ files/pages/site.php
@@ -1,17 +1,40 @@
+<!-- BEGIN CONTENT -->
+<div class="page-content-wrapper site hidden">
+    <!-- BEGIN CONTENT BODY -->
+    <div class="page-content">
 
-                <!-- BEGIN CONTENT -->
-                <div class="page-content-wrapper site hidden">
-                    <!-- BEGIN CONTENT BODY -->
-                    <div class="page-content">
-                        <!-- BEGIN PAGE HEADER-->
-                        <!-- BEGIN PAGE TITLE-->
-                        <h1 class="page-title">Manage Sites</h1>
-                        <!-- END PAGE TITLE-->
-                        <!-- END PAGE HEADER-->
-                        <div class="note note-info">
-                            <p> A black page template with a minimal dependency assets to use as a base for any custom page you create </p>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="portlet">
+                    <div class="portlet-title">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <select class="form-control site-selection select2">
+                                        <option value="">Select Website</option>
+                                        <option value="">Option 1</option>
+                                        <option value="">Option 2</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="actions pull-right">
+                                    <button class="btn green">Add New</button>
+                                    <button class="btn red">Delete</button>
+                                    <button class="btn green">Save</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <!-- END CONTENT BODY -->
+
+                    <div class="portlet-title">
+                    </div>
+
                 </div>
-                <!-- END CONTENT -->
+            </div>
+        </div>
+
+    </div>
+    <!-- END CONTENT BODY -->
+</div>
+<!-- END CONTENT -->
