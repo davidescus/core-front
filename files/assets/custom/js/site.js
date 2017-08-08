7d128767071135007f7496bf455a7bdbf09b74b7
davidescus <rahthman_s@yahoo.com>
2017-08-08 09:59:43 +0000

    Add method to show site info an start and on change site selection

--- files/assets/custom/js/site.js
+++ files/assets/custom/js/site.js
@@ -1,6 +1,17 @@
 config.site = $('.page-content-wrapper.site');
 
 /*
+ * Click actions
+ */
+
+    /*
+    * when change site selection get site information
+    */
+$(config.site).on('change', '.site-selection', function(){
+    showSiteInfo($(this).val());
+});
+
+/*
  * this will populate website selection
  */
 function getAllSitesIdsAndNames() {
@@ -9,8 +20,6 @@ function getAllSitesIdsAndNames() {
         url: config.coreUrl + "/site/ids-and-names",
         type: "get",
         success: function (response) {
-
-            console.log(response);
 
             var data = {
                 sites: response,
@@ -21,9 +30,63 @@ function getAllSitesIdsAndNames() {
             var compiledTemplate = Template7.compile(template);
             var html = compiledTemplate(data);
             element.find('.site-selection').html(html);
+
+            showSiteInfo(response[0].id);
+        },
+         error: function () {}
+    });
+}
+
+function showSiteInfo(siteId) {
+    $.ajax({
+        url: config.coreUrl + "/site/" + siteId,
+        type: "get",
+        success: function (response) {
+
+            console.log(response);
+
+            // show site name in front;
+            showSiteName(response);
+            showSiteGeneralConfiguration(response);
         },
          error: function () {
 
         }
     });
 }
+
+function showSiteName(data) {
+    var element = config.site;
+    var template = element.find('.template-site-name').html();
+    var compiledTemplate = Template7.compile(template);
+    var html = compiledTemplate(data);
+    element.find('.site-name').html(html);
+}
+
+function showSiteGeneralConfiguration(data) {
+    var element = config.site;
+    var template = element.find('.template-site-name').html();
+    var compiledTemplate = Template7.compile(template);
+    var html = compiledTemplate(data);
+    element.find('.site-name').html(html);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
