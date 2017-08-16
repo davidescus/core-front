6c3b483e30e5f9ae9126f402644b0b6659d0eaac
davidescus <rahthman_s@yahoo.com>
2017-08-16 17:13:37 +0000

    Clear php script for typeahead

--- files/index.php
+++ files/index.php
@@ -1,19 +1,19 @@
 <?php include "common/header.php" ?>
 
-            <!-- BEGIN CONTAINER -->
-            <div class="page-container">
+        <!-- BEGIN CONTAINER -->
+        <div class="page-container">
 
-                <?php include "common/menu.php" ?>
+            <?php include "common/menu.php" ?>
 
-                <!-- start pages -->
-                <?php include "pages/site.php" ?>
-                <?php include "pages/association.php" ?>
-                <?php include "pages/distribution.php" ?>
-                <?php include "pages/archive-big.php" ?>
+            <!-- start pages -->
+            <?php include "pages/site.php" ?>
+            <?php include "pages/association.php" ?>
+            <?php include "pages/distribution.php" ?>
+            <?php include "pages/archive-big.php" ?>
 
-                <?php include "common/sidebar.php" ?>
+            <?php include "common/sidebar.php" ?>
 
-            </div>
-            <!-- END CONTAINER -->
+        </div>
+        <!-- END CONTAINER -->
 
 <?php include "common/footer.php" ?>
