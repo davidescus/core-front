92525396e0880e5e108dcae1041783d36b6cc29f
davidescus <rahthman_s@yahoo.com>
2017-08-28 16:45:05 +0000

    Add for moment here function that will manage ajax errors.

--- files/example.config.js
+++ files/example.config.js
@@ -7,3 +7,12 @@ var config = {
     coreUrl: "http://domain | ip/admin",
     activePage: "association",
 };
+
+// check if response server is ok,
+// if not redirect to login
+function manageError(a, b, c) {
+
+    var href = document.location.href.split("?")[0];
+    window.location.replace(href + "?logout=true");
+    return;
+}
