6c4ae91a5d49bbeae389e5c4ee5adeabfe39de35
davidescus <rahthman_s@yahoo.com>
2017-08-28 11:51:28 +0000

    Implement login in front.

--- files/index.php
+++ files/index.php
@@ -1,20 +1,90 @@
-<?php include "common/header.php" ?>
+<?php
+    session_start();
 
-        <!-- BEGIN CONTAINER -->
-        <div class="page-container">
+    // logout user
+    if  (isset($_GET['logout'])) {
 
-            <?php include "common/menu.php" ?>
+        $_SESSION['loggedIn'] = false;
 
-            <!-- start pages -->
-            <?php include "pages/site.php" ?>
-            <?php include "pages/event.php" ?>
-            <?php include "pages/association.php" ?>
-            <?php include "pages/distribution.php" ?>
-            <?php include "pages/archive-big.php" ?>
+        echo '<script type="text/javascript">
 
-            <?php include "common/sidebar.php" ?>
+                // remove token from localStorage
+                localStorage.removeItem("core-app-token-x76");
 
-        </div>
-        <!-- END CONTAINER -->
+                // refresh location without querystring.
+                var href = document.location.href.split("?")[0];
+                window.location.replace(href);
 
-<?php include "common/footer.php" ?>
+             </script>';
+    }
+
+    // user was logged in , so serve dashboard
+    if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']) {
+        include "common/header.php";
+
+            echo '<div class="page-container">';
+
+            include "common/menu.php";
+
+            include "pages/site.php";
+            include "pages/event.php";
+            include "pages/association.php";
+            include "pages/distribution.php";
+            include "pages/archive-big.php";
+
+            include "common/sidebar.php";
+
+            echo '</div>';
+
+        include "common/footer.php";
+    }
+
+    // perform login action
+    if (isset($_POST['login'])) {
+
+        // perform login operation by curl
+        $url = "http://51.15.78.71/login";
+        $data = [
+            'email' => $_POST['email'],
+            'password' => $_POST['password'],
+        ];
+        $post = curl_init();
+        curl_setopt($post, CURLOPT_URL, $url);
+        curl_setopt($post, CURLOPT_POST, count($data));
+        curl_setopt($post, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($post, CURLOPT_RETURNTRANSFER, true);
+        $ret = curl_exec($post);
+
+        $response = json_decode($ret, true);
+
+        // success autentificate
+        if ($response['success']) {
+            $_SESSION['loggedIn'] = true;
+
+            // send script to set in local storage token
+            // also to perform an redirect here (user is logged in now)
+            echo '<script type="text/javascript">
+
+                    // set token in localStorage
+                    localStorage.setItem("core-app-identifier-x76", "' . $response['token'] . '");
+
+                    // refresh location without querystring.
+                    var href = document.location.href.split("?")[0];
+                    window.location.replace(href);
+
+                 </script>';
+
+            die();
+        }
+    }
+?>
+
+<!-- show login form -->
+<form action="" method="post">
+    Email:
+    <input type="text" name="email">
+    <br/>
+    Password
+    <input type="text" name="password">
+    <input type="submit" name="login" value="login">
+</form>
